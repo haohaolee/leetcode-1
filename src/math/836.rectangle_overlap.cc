30f7b90eaabf5068a4189fcdee144037dbba4dd2
Su Shi <1684739+metacpp@users.noreply.github.com>
2020-01-11 12:17:26 -0800

    Init work for 836.

--- src/math/836.rectangle_overlap.cc
+++ src/math/836.rectangle_overlap.cc
@@ -7,6 +7,7 @@ namespace math {
 
 using namespace std;
 
+// TODO: let's start to work on it.
 bool RectangleOverlap::IsOverlap(std::vector<int>& rec1, std::vector<int>& rec2) {
   return false;
 }
