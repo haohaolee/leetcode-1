55bb1a2740dd95222c13454a3fbb833c11227375
Su Shi <1684739+metacpp@users.noreply.github.com>
2020-01-12 13:24:30 -0800

    add solution for 836.

--- src/math/836.rectangle_overlap.cc
+++ src/math/836.rectangle_overlap.cc
@@ -7,9 +7,13 @@ namespace math {
 
 using namespace std;
 
-// TODO: let's start to work on it.
 bool RectangleOverlap::IsOverlap(std::vector<int>& rec1, std::vector<int>& rec2) {
-  return false;
+  if (rec1.size() != 4 || rec2.size() != 4) return false;
+
+  bool xOverlap = rec1[0] < rec2[2] && rec2[0] < rec1[2];
+  bool yOverlap = rec1[1] < rec2[3] && rec2[1] < rec1[3];
+
+  return xOverlap && yOverlap;
 }
 
 }  // namespace math
