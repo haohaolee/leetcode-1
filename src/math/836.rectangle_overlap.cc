c0d2908db45e04928e9e109695982104d3baa4cc
Su Shi <1684739+metacpp@users.noreply.github.com>
2020-01-12 13:51:13 -0800

    rename the variables.

--- src/math/836.rectangle_overlap.cc
+++ src/math/836.rectangle_overlap.cc
@@ -10,10 +10,10 @@ using namespace std;
 bool RectangleOverlap::IsOverlap(std::vector<int>& rec1, std::vector<int>& rec2) {
   if (rec1.size() != 4 || rec2.size() != 4) return false;
 
-  bool xOverlap = rec1[0] < rec2[2] && rec2[0] < rec1[2];
-  bool yOverlap = rec1[1] < rec2[3] && rec2[1] < rec1[3];
+  bool x_overlap = rec1[0] < rec2[2] && rec2[0] < rec1[2];
+  bool y_overlap = rec1[1] < rec2[3] && rec2[1] < rec1[3];
 
-  return xOverlap && yOverlap;
+  return x_overlap && y_overlap;
 }
 
 }  // namespace math
